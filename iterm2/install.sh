2352a793a73c99e977c5d6ee9de2a46c985eb9a4
Semoz <lee80386@hotmail.com>
2017-06-09 16:03:33 +0800

    小改一下，尝试硬链iterms

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -8,5 +8,5 @@ cd "$(dirname "$0")/.." # 定位到执行当前脚本的目录上级
 
 sudo ln -s -f ~/DotFiles/iterm2/item2login.sh /usr/local/bin/item2login.sh # 软链接文件
 
-
-#sudo ln -s -f ~/DotFiles/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+# 硬链文件 需要安装好drowbox同步后再执行
+# sudo ln -f ~/Dropbox/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
---
+++ sharedata/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Run all dotfiles installers.
+
+set -e
+
+cd "$(dirname "$0")/.." # 定位到执行当前脚本的目录上级
+
+sudo ln -s -f ~/Dropbox/keePass ~/Documents/SharedData/keePass
