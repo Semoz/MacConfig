db282113d4c9fe7991926e942ddbc2a43fc0c4a8
Semoz <lee80386@hotmail.com>
2018-01-17 20:57:00 +0800

    update fcp

--- MacConfig/LinkFiles/Config/FinalCutPro.sh
+++ MacConfig/LinkFiles/Config/FinalCutPro.sh
@@ -5,6 +5,6 @@ ROOT_PATH=$(pwd -P)
 source message
 
 info "> Link FCP Effects.."
-DESTINATION_PATH=$HOME"/Movies/Motion Templates.localized/Effects"
-SOURCE_PATH=$ROOT_PATH"/FileData/FinalCutPro/Effects"
+DESTINATION_PATH=$HOME"/Movies/Motion Templates.localized/Effects.localized"
+SOURCE_PATH=$ROOT_PATH"/FileData/FinalCutPro/Effects.localized"
 link_data_path "$SOURCE_PATH" "$DESTINATION_PATH"
