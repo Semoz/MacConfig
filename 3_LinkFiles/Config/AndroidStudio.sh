afc38e713be1eb2fd9031f9e42afe5d15b861a30
Semoz <mengzhang18@gmail.com>
2020-05-21 11:46:37 +0800

    Update AndroidStudio.sh

--- 3_LinkFiles/Config/AndroidStudio.sh
+++ 3_LinkFiles/Config/AndroidStudio.sh
@@ -8,3 +8,8 @@ source message
 DESTINATION_PATH=$HOME"/Library/Android/sdk"
 SOURCE_PATH=$HOME"/_AppData/Android Studio/sdk"
 link_data_path "$SOURCE_PATH" "$DESTINATION_PATH"
+
+# Link Android Gradle File
+DESTINATION_PATH=$HOME"/.gradle"
+SOURCE_PATH=$HOME"/_AppData/Android Studio/gradle"
+link_data_path "$SOURCE_PATH" "$DESTINATION_PATH"
